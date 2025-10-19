107b25617f5b05a3b5354999f0252c872e5fa112
ReactNative2000 <ReactNative2000@users.noreply.github.com>
2025-10-19 16:43:31 +0000

    deploy: 6bf0d5cd27f819ba723d004eae8314e8dac5ff74

---
+++ assets/cash-config.js
@@ -0,0 +1,12 @@
+// This file is generated to provide cash config to static docs pages.
+(function(global){
+  // Keep values in sync with /config/cash.json
+  global.cashConfig = {
+    cashtag: '$brandon314314',
+    utm: {
+      source: 'repo',
+      medium: 'docs',
+      campaign: 'support'
+    }
+  };
+})(this);
